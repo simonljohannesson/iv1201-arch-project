0e4a7bd57033b1bf4d933ee0b56ada8bef47421c
virsp <victor.pettersson@gmail.com>
2022-02-02 15:50:50 +0100

    intellij hot reloading with docker

--- recruitmentApplication/src/main/java/se/kth/iv1201/group6/recruitmentApplication/RecruitmentApplication.java
+++ recruitmentApplication/src/main/java/se/kth/iv1201/group6/recruitmentApplication/RecruitmentApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RecruitmentApplication {
 
 	public static void main(String[] args) {
+		System.out.println("live working");
 		SpringApplication.run(RecruitmentApplication.class, args);
 	}
 
