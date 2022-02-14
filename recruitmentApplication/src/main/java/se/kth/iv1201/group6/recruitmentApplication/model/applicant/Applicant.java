1dd0a80f68c8bb003a0cf014f2339a420af7ae47
virsp <victor.pettersson@gmail.com>
2022-02-14 15:04:37 +0100

    base applicant class, may want to add more data field later

---
+++ recruitmentApplication/src/main/java/se/kth/iv1201/group6/recruitmentApplication/model/applicant/Applicant.java
@@ -0,0 +1,71 @@
+package se.kth.iv1201.group6.recruitmentApplication.model.applicant;
+
+
+import javax.persistence.*;
+
+/**
+ * @author Victor Pettersson last changed 2022-02-14
+ */
+@Entity
+@Table(name = "person")
+public class Applicant {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "person_id")
+    private Long id;
+
+    private String name;
+    @Column(name = "surname")
+    private String lastName;
+    private String pnr;
+    private String email;
+    private String username;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPnr() {
+        return pnr;
+    }
+
+    public void setPnr(String pnr) {
+        this.pnr = pnr;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
