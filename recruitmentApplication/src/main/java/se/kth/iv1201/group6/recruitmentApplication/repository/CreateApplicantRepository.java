245fe53c8a65ec8f0dc8ec665d25beb0fe877d92
virsp <victor.pettersson@gmail.com>
2022-02-17 11:49:08 +0100

    a seperation was, due to extending different repos

--- recruitmentApplication/src/main/java/se/kth/iv1201/group6/recruitmentApplication/repository/CreateApplicantRepository.java
+++ recruitmentApplication/src/main/java/se/kth/iv1201/group6/recruitmentApplication/repository/CreateApplicantRepository.java
@@ -1,28 +1,20 @@
 package se.kth.iv1201.group6.recruitmentApplication.repository;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import se.kth.iv1201.group6.recruitmentApplication.model.applicant.Applicant;
+import se.kth.iv1201.group6.recruitmentApplication.dao.CreateApplicantDao;
 
-/**
- * @author Victor Pettersson last changed 2022-02-14
- */
-@Repository
-public interface ApplicantRepository extends PagingAndSortingRepository<Applicant, Long> {
+import java.util.Collection;
+import java.util.Optional;
 
-    @Query("SELECT a FROM Applicant a WHERE role_id = 2")
-    Page<Applicant> findAllApplicants(Pageable pageable);
-}
 @Repository
-public interface ApplicantRepository extends CrudRepository<CreateApplicantDao, Long> {
+public interface CreateApplicantRepository extends CrudRepository<CreateApplicantDao, Long>{
     @Query("SELECT u FROM CreateApplicantDao u WHERE u.username = :username OR u.pnr = :pnr OR u.email = :email")
     Optional<Collection<CreateApplicantDao>> findByUniqueFields(
             @Param("username")  String username,
             @Param("pnr")       String pnr,
             @Param("email")     String email);
-
 }
 
