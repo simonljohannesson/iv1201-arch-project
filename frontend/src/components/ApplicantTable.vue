00b79b323b344753d256bb23d2fae74d1b49b791
virsp <victor.pettersson@gmail.com>
2022-02-17 18:18:37 +0100

    display error and renaming

--- frontend/src/components/ApplicantTable.vue
+++ frontend/src/components/ApplicantTable.vue
@@ -5,10 +5,24 @@
 
         </div>
         <div class="row">
-            <b-table striped hover :items="a"></b-table>
+            <b-table striped hover :items="list" :busy="loading">
+                <template #table-busy>
+                    <div v-if="!loadingError" class="text-center text-primary my-2">
+                        <b-spinner class="align-middle"></b-spinner>
+                        <strong>Loading...</strong>
+                    </div>
+                    <div v-else class="text-danger">
+                        <b-icon icon="exclamation-circle-fill"></b-icon>
+                        <strong>Something went wrong, try again by reloading</strong>
+                    </div>
+                </template>
+            </b-table>
         </div>
         <div class="row">
-            <b-pagination v-model="currentPage" :total-rows="rows" :per-page="perPage" aria-controls="my-table" @input="load()"></b-pagination>
+            <div class="col-auto">
+                <b-form-select v-model="selected" :options="quantityOptions" @input="loadApplicants()"></b-form-select>
+            </div>
+            <b-pagination v-model="currentPage" :total-rows="rows" :per-page="perPage" aria-controls="my-table" @input="loadApplicants()"></b-pagination>
         </div>
     </div>
 </template>
@@ -19,27 +33,37 @@ export default {
     props: ['applicants'],
     data () {
         return {
-            a: [],
+            list: [],
             currentPage: 1,
             realPage: 0,
             rows: 100,
-            perPage: 10
+            perPage: 10,
+            loading: false,
+            selected: 10,
+            quantityOptions: [10, 20, 50, 100],
+            loadingError: false
         };
     },
     methods: {
         ...mapActions(['getApplicants']),
-        async load () {
-            const app = await this.getApplicants(this.currentPage - 1);
-            console.log(app.pageable.pageNumber);
-            this.a = app.content;
-            this.realPage = app.pageable.pageNumber;
-            this.currentPage = this.realPage + 1;
-            this.rows = app.totalElements;
-            this.perPage = app.size;
+        async loadApplicants () {
+            this.loadingError = false;
+            this.loading = true;
+            try {
+                const app = await this.getApplicants({ page: this.currentPage - 1, size: this.selected });
+                this.list = app.content;
+                this.realPage = app.pageable.pageNumber;
+                this.currentPage = this.realPage + 1;
+                this.rows = app.totalElements;
+                this.perPage = app.size;
+                this.loading = false;
+            } catch (error) {
+                this.loadingError = true;
+            }
         }
     },
     mounted () {
-        this.load();
+        this.loadApplicants();
     }
 };
 </script>
