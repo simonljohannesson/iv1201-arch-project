35a1b13316018e9f2ed14cd5b6f2b9931ca253d2
virsp <victor.pettersson@gmail.com>
2022-02-17 18:33:35 +0100

    navbar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,30 @@
 <template>
-  <div id="app" class="container-fluid">
-    <div id="nav">
-      <router-link to="/">First</router-link>
-    </div>
-    <router-view/>
-  </div>
-</template>
+    <div id="app">
+        <b-navbar toggleable="lg" type="dark" variant="dark">
+            <b-navbar-brand @click="toHomeView()">Recruitment Application</b-navbar-brand>
+            <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+            <b-collapse id="nav-collapse" is-nav>
+                <b-navbar-nav>
+                <b-nav-item><router-link tag="li" to="/applicants">Applicant</router-link></b-nav-item>
+                </b-navbar-nav>
+            </b-collapse>
+        </b-navbar>
 
+        <!-- Page content under navbar -->
+        <div class="container-fluid">
+            <router-view/>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    methods: {
+        toHomeView () {
+            this.$router.push('/');
+        }
+    }
+};
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -28,4 +46,12 @@
 #nav a.router-link-exact-active {
   color: #42b983;
 }
+nav li:hover,
+nav li.router-link-active,
+nav li.router-link-exact-active {
+   /* background-color: rgb(52, 159, 201); */
+   font-weight: bolder;
+
+   cursor: pointer;
+ }
 </style>
