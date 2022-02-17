dd2c360183f906c26c14aa7f6ab90b0a85fff43c
virsp <victor.pettersson@gmail.com>
2022-02-17 11:23:11 +0100

    basic store functionality for applicants

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,14 +1,25 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import { applicants as applicantDB } from '../api/applicants';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
     state: {
+        applicants: []
     },
     mutations: {
+        setApplicants (state, applicants) {
+            state.applicants = applicants;
+        }
     },
     actions: {
+        async getApplicants (state, page, size) {
+            const applicants = await applicantDB.get(page, size);
+            console.log(applicants);
+            state.commit('setApplicants', applicants);
+            return applicants;
+        }
     },
     modules: {
     }
