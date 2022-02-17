166a4800bc41979aa1ddabdc2d9a4a06a40a3fd2
virsp <victor.pettersson@gmail.com>
2022-02-17 11:23:30 +0100

    shows the applicant table

--- frontend/src/views/First.vue
+++ frontend/src/views/First.vue
@@ -1,19 +1,15 @@
 <template>
     <div class="container justify-content-center">
         <div class="row">
-            <h1 class="col">Init done</h1>
-
-        </div>
-        <div class="row">
-            <b-iconstack class="col" animation="spin">
-                <b-icon icon="check-circle" animation="throb" font-scale="4"></b-icon>
-            </b-iconstack>
-
+            <applicant-table/>
         </div>
     </div>
 </template>
 <script>
+// import ApplicantTable from '@/components/ApplicantTable.vue'
+import ApplicantTable from '../components/ApplicantTable.vue';
 export default {
+    components: { ApplicantTable },
     name: 'First'
 };
 </script>
