4fc300d7237f55b1b06aa2df8f3210b973554841
Simon Johannesson <simonljohannesson@gmail.com>
2022-02-22 11:47:57 +0100

    cors, csrf and login settings

---
+++ recruitmentApplication/src/main/java/se/kth/iv1201/group6/recruitmentApplication/controller/LoginController.java
@@ -0,0 +1,21 @@
+package se.kth.iv1201.group6.recruitmentApplication.controller;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+
+@RestController
+public class LoginController {
+
+    @PreAuthorize("hasAnyRole('ROLE_RECRUITER')")
+    @GetMapping("/user")
+    public Object user(Principal user) {
+        return new Object(){
+            public final boolean authenticated = true;
+        };
+    }
+
+}
